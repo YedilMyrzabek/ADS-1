874c69212802df2de7e21daf6398dd0c5ed805f7
yedil <230760@astanait.edu.kz>
2024-04-09 22:24:03 +0500

    First

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(first(4));
+    }
+    public static int first(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        int y = x * x;
+        return y + first(x - 1);
+    }
+}
