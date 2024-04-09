df649784d72865ab9e2dac220803c4ec0e478156
yedil <230760@astanait.edu.kz>
2024-04-09 22:59:35 +0500

    Eighth

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        sixth(sc,n);
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+
+        eighth(x, y, new ArrayList<>());
     }
-    public static void sixth(Scanner sc,int n){
+    public static void eighth(int n, int k, List<Integer> nums) {
         if (n == 0) {
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
             return;
         }
-        else {
-            String elem = sc.next();
 
-            sixth(sc, n - 1);
-            System.out.println(elem);
+        for (int i = 1; i <= k; i++) {
+            nums.add(i);
+            eighth(n - 1, k, nums);
+            nums.remove(nums.size() - 1);
         }
     }
 }
