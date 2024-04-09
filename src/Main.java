1eff86ea56223a36a90f7b3f682ef5f000f93d92
yedil <230760@astanait.edu.kz>
2024-04-09 23:21:01 +0500

    Tenth

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        int x = 2;
-        int y = 3;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        eighth(x, y, new ArrayList<>());
+        if(tenth(n)){
+            System.out.println(n + " is power of two");
+        }
+        else {
+            System.out.println(n + " is not power of two");
+        }
     }
-    public static void eighth(int n, int k, List<Integer> nums) {
-        if (n == 0) {
-            for (int num : nums) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            return;
+    public static boolean tenth(int n) {
+        if (n <= 0) {
+            return false;
         }
 
-        for (int i = 1; i <= k; i++) {
-            nums.add(i);
-            eighth(n - 1, k, nums);
-            nums.remove(nums.size() - 1);
+        while (n > 1) {
+            if (n % 2 != 0) {
+                return false;
+            }
+            n /= 2;
         }
+        return true;
     }
 }
