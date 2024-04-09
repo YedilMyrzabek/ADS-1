2209ca37aed9caa8cd951bd0e94561e89984221f
yedil <230760@astanait.edu.kz>
2024-04-09 22:28:22 +0500

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(first(4));
+        int[] arr = {2,3,4,1,2,4};
+        int n = 4;
+        System.out.println(first(arr,n));
     }
-    public static int first(int x) {
-        if (x == 1) {
-            return 1;
+    public static int first(int[] arr,int n){
+        if(arr.length == 1 || n == 1){
+            return arr[0];
         }
-        int y = x * x;
-        return y + first(x - 1);
+        return arr[n-1] + first(arr,n-1);
     }
 }
