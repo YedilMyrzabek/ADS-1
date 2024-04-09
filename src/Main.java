c84cfdb27373ed7941b11a958a99776feee14c2f
yedil <230760@astanait.edu.kz>
2024-04-09 22:44:34 +0500

    Fifth

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int b = 4;
-        int n = 3;
-        System.out.println(first(b,n));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        first(sc,n);
     }
-    public static int first(int x,int n){
+    public static void first(Scanner sc,int n){
         if(n == 0){
-            return 1;
+            return;
         }
-        int y = (int) Math.pow(x,n);
-        return y+first(x,n-1);
+
+        int num = sc.nextInt();
+
+        first(sc,n-1);
+        System.out.println(num);
     }
 }
