c4bea1a5587beabadf5d942c6639b83cd7a019d3
yedil <230760@astanait.edu.kz>
2024-04-09 22:37:19 +0500

    Third

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,1,2,4};
+        int[] arr = {2,3,-1,1,2,4};
         int n = 4;
         System.out.println(first(arr,n));
     }
     public static int first(int[] arr,int n){
-        if(arr.length == 1 || n == 1){
+        if(n == 1){
             return arr[0];
         }
-        return arr[n-1] + first(arr,n-1);
+        if(arr[n-1]>0) {
+            return arr[n-1] + first(arr, n - 1);
+        }else {
+            return first(arr,n-1);
+        }
     }
 }
